09eaa9d21475794820a8115f1fd7dbd522ffbc92
Pedro-V <mingas62@hotmail.com>
2022-08-05 06:32:51 -0300

    Primeiro teste funcional

---
+++ superlists/functional_tests.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import unittest
+
+# Todo script que usa o webdriver deve ativar o server rodando, na linha de comando: 
+# python manage.py runserver
+
+class NewVisitorTest(unittest.TestCase):
+    # setUp e tearDown rodam antes e após cada teste
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
