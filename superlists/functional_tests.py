8a049e95c8039892fcfd7a645887e038802b0bb7
Pedro-V <mingas62@hotmail.com>
2022-08-07 10:50:32 -0300

    Django test client agora gera a front page usando um template HTML

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
@@ -35,7 +35,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         self.fail('Finish the test!')
